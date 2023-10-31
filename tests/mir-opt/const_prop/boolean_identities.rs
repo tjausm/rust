5762c805170d18c8966190bc99052d49e0d73962
T S <t.a.smid@students.uu.nl>
2023-10-31 12:01:22 +0100

    Add two more tests

--- tests/mir-opt/const_prop/boolean_identities.rs
+++ tests/mir-opt/const_prop/boolean_identities.rs
@@ -1,9 +1,11 @@
-// skip-filecheck
 // unit-test: ConstProp
 // compile-flags: -O -Zmir-opt-level=4
 
 // EMIT_MIR boolean_identities.test.ConstProp.diff
 pub fn test(x: bool, y: bool) -> bool {
+    // Verify that boolean operators are solved (if possible)
+    // CHECK-LABEL: fn test(
+    // CHECK: _0 = const false;
     (y | true) & (x & false)
 }
 
