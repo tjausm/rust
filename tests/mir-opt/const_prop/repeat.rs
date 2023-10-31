223b2f1ae184ecc5875d6b399f04b53991bb3fa4
T S <t.a.smid@students.uu.nl>
2023-10-31 16:24:40 +0100

    add two more tests

--- tests/mir-opt/const_prop/repeat.rs
+++ tests/mir-opt/const_prop/repeat.rs
@@ -1,9 +1,10 @@
-// skip-filecheck
 // unit-test: ConstProp
 // EMIT_MIR_FOR_EACH_PANIC_STRATEGY
 // EMIT_MIR_FOR_EACH_BIT_WIDTH
 
 // EMIT_MIR repeat.main.ConstProp.diff
 fn main() {
+    // CHECK-LABEL: fn main(
+    // CHECK: {{_[0-9]+}} = const 42_u32;
     let x: u32 = [42; 8][2] + 0;
 }
